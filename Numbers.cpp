6768001dc4fd65a0c6837eb900915947d84c1d18
Вовус <nikitin.vld@gmail.com>
2023-07-14 15:28:32 +0600

    numnum

---
+++ Numbers.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+
+const int N = 10;
+
+void printNumbers(bool printEven, int limit) {
+
+    int start = printEven ? 0 : 1;
+
+    for (int i = start; i <= limit; i++) {
+
+        if ((i % 2 == 0 && printEven) || (i % 2 != 0 && !printEven)) {
+
+            std::cout << i << " ";
+        }
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    
+    for (int i = 0; i <= N; i += 2) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+
+    printNumbers(false, N);
+
+    return 0;
+}
+
